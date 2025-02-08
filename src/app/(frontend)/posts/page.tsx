ef93dca48f8c7eef85126a0b7504314b269ee48a
[Powerplant] <hotline@powerplant.design>
2025-02-08 19:10:17 +1300

    Add Images and Portable Text

--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -10,7 +10,7 @@ export default async function Page() {
     return (
         <main className='container mx-auto grid grid-cols-1 gap-6 p-12'>
             <h1 className='text-4xl font-bold'>Post index</h1>
-            <ul className='grid grid-cols-1 divide-y divide-green-100'>
+            <ul className='grid grid-cols-2 divide-y divide-green-100'>
                 {posts.map((post) => (
                     <li key={post._id}>
                         <Link
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
-export default {
+const config: Config = {
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,5 +9,7 @@ export default {
         "./src/sanity/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {},
-    plugins: [],
-} satisfies Config;
+    plugins: [typography],
+};
+
+export default config;
